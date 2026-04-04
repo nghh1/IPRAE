5adeb060889e7be37cdd01f1bd31bc47ee66aeee
nghh1 <0209ivanng@gmail.com>
2026-04-04 23:24:26 +0100

    Update db related issue

--- api.py
+++ api.py
@@ -37,8 +37,13 @@ class SimulationRequest(BaseModel):
     mean_shock: float
     rebalance: bool
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./iprae_community.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./iprae_community.db")
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
